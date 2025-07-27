d691644b7d63796914d8947ab6e3014bd2d79da4
boggs 🌊 <6130147+b-ggs@users.noreply.github.com>
2025-07-27 21:42:13 +0800

    Set conn_max_age and conn_health_checks

--- kalokohan/settings/base.py
+++ kalokohan/settings/base.py
@@ -84,7 +84,11 @@ WSGI_APPLICATION = "kalokohan.wsgi.application"
 
 if "DATABASE_URL" in os.environ:
     DATABASES = {
-        "default": dj_database_url.parse(os.environ["DATABASE_URL"]),
+        "default": dj_database_url.parse(
+            os.environ["DATABASE_URL"],
+            conn_max_age=600,
+            conn_health_checks=True,
+        ),
     }
 
 
